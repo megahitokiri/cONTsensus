739f5e5eeb8f4b9dda060c3a7aeef9b444cddce5
megahitokiri <jose.lazaroguevara@utah.edu>
2022-04-18 20:31:05 -0700

    version 0.94

--- scripts/GGC_BLAST_name_correction.R
+++ scripts/GGC_BLAST_name_correction.R
@@ -1,4 +1,5 @@
 library (dplyr)
+library(stringr)
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -9,6 +10,32 @@ BLAST_results <- read.table(input_file, header = FALSE)
 
 colnames(BLAST_results) <- c("qseqid", "evalue", "qcovhsp", "salltitles", "pident")
 
-BLAST_results$salltitles <- gsub("_"," ",BLAST_results$salltitles)
+BLAST_results$salltitles <- sub("_"," ",BLAST_results$salltitles)
+BLAST_results$Word_count <- str_count(BLAST_results$salltitles,"\\w+")
+BLAST_results$salltitles <-ifelse(BLAST_results$Word_count==1,
+                                  paste0(BLAST_results$salltitles," ",BLAST_results$salltitles),
+                                  BLAST_results$salltitles)
+
+BLAST_results$word1 <- word(BLAST_results$salltitles,1)
+BLAST_results$word2 <- word(BLAST_results$salltitles,2)
+
+BLAST_results$salltitles <-ifelse(BLAST_results$word1=="Candidatus",
+                                  gsub("_"," ",BLAST_results$salltitles),
+                                  BLAST_results$salltitles)
+
+BLAST_results$Word_count <- str_count(BLAST_results$salltitles,"\\w+")
+
+BLAST_results$Candidatus_fix <-ifelse(BLAST_results$Word_count==2&BLAST_results$word1=="Candidatus",
+                                  TRUE,
+                                  FALSE)
+BLAST_results$Candidatus_fix <-ifelse(BLAST_results$Candidatus_fix==TRUE,
+                                      paste0(BLAST_results$salltitles," ",BLAST_results$word2),
+                                      BLAST_results$salltitles)
+
+BLAST_results$salltitles <- BLAST_results$Candidatus_fix
+BLAST_results$Word_count <-NULL
+BLAST_results$word1 <- NULL
+BLAST_results$word2 <- NULL
+BLAST_results$Candidatus_fix <- NULL
 
 write.table(BLAST_results,file=output_file, quote = FALSE, sep ="\t", row.names = FALSE, col.names = FALSE, na ="")
